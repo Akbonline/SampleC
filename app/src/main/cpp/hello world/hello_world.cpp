89d376e698d34fba4d7c06d34e093d24ea177385
Akshat Bajpai <akshatbajpai.biz@gmail.com>
2021-07-14 13:58:11 -0700

    Version 2

---
+++ app/src/main/cpp/hello world/hello_world.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Akshat Bajpai on 7/14/21.
+//
+#include<iostream>
+#include<string>
+using namespace std;
+
+string returnHello(){
+    return "Hello world";
+}
+
+
--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,12 +1,13 @@
 #include <jni.h>
 #include <string>
 #include "dateTime.h"
+#include "hello world/hello_world.cpp"
 
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_samplec_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++, The Date and Time is: "+getDateTime();
+    std::string hello = returnHello()+" from C++, The Date and Time is: "+getDateTime();
     return env->NewStringUTF(hello.c_str());
 }
